cce59bf180950be78436c259c776d2f248d1a7b8
cremeni25 <andersonnavarro.si@gmail.com>
2026-09-12 17:40:45 -0300

    Integra motor individual longitudinal v3

--- app/main.py
+++ app/main.py
@@ -28,6 +28,7 @@ from app.project_management import router as project_management_router
 from app.technical_team_management import router as technical_team_management_router
 from app.athlete_technician_management import router as athlete_technician_management_router
 from app.athlete_intelligence import router as athlete_intelligence_router
+from app.individual_intelligence_v3 import router as individual_intelligence_v3_router
 
 LOGGER = logging.getLogger("agp.startup")
 
@@ -46,6 +47,7 @@ app.include_router(project_management_router)
 app.include_router(technical_team_management_router)
 app.include_router(athlete_technician_management_router)
 app.include_router(athlete_intelligence_router)
+app.include_router(individual_intelligence_v3_router)
 
 app.add_middleware(
     CORSMiddleware,
