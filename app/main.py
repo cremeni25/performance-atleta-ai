9283b7d02e5103d73da18b21f0c52dba0a24b5c2
cremeni25 <andersonnavarro.si@gmail.com>
2026-09-16 18:00:19 -0300

    AGP: register canonical longitudinal intelligence router

--- app/main.py
+++ app/main.py
@@ -32,6 +32,7 @@ from app.athlete_intelligence import router as athlete_intelligence_router
 from app.individual_intelligence_v4 import router as individual_intelligence_v4_router
 from app.multidisciplinary_decision import router as multidisciplinary_decision_router
 from app.identity_context import router as identity_context_router
+from app.canonical_longitudinal_intelligence import router as canonical_longitudinal_intelligence_router
 
 LOGGER = logging.getLogger("agp.startup")
 
@@ -54,6 +55,7 @@ app.include_router(athlete_intelligence_router)
 app.include_router(individual_intelligence_v4_router)
 app.include_router(multidisciplinary_decision_router)
 app.include_router(identity_context_router)
+app.include_router(canonical_longitudinal_intelligence_router)
 
 app.add_middleware(
     CORSMiddleware,
