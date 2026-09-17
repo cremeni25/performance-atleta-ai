625f287d9ba783dcc6ad9f947a747a9fd99bf811
cremeni25 <andersonnavarro.si@gmail.com>
2026-09-17 00:29:26 -0300

    AGP: register canonical training competition router

--- app/main.py
+++ app/main.py
@@ -34,6 +34,7 @@ from app.canonical_longitudinal_intelligence import router as canonical_longitud
 from app.canonical_institutional_intelligence import router as canonical_institutional_intelligence_router
 from app.integrative_ai_context import router as integrative_ai_context_router
 from app.canonical_decision_cycle import router as canonical_decision_cycle_router
+from app.canonical_training_competition import router as canonical_training_competition_router
 
 app = FastAPI()
 app.include_router(owner_activation_router)
@@ -58,6 +59,7 @@ app.include_router(canonical_longitudinal_intelligence_router)
 app.include_router(canonical_institutional_intelligence_router)
 app.include_router(integrative_ai_context_router)
 app.include_router(canonical_decision_cycle_router)
+app.include_router(canonical_training_competition_router)
 
 app.add_middleware(
     CORSMiddleware,
