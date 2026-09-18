90ad4d1576eae23bbaf636ff1556e32054be41d3
cremeni25 <andersonnavarro.si@gmail.com>
2026-09-18 08:07:38 -0300

    AGP Swimming: activate coach athlete invitation API

--- app/main.py
+++ app/main.py
@@ -38,6 +38,7 @@ from app.integrative_ai_context import router as integrative_ai_context_router
 from app.canonical_decision_cycle import router as canonical_decision_cycle_router
 from app.canonical_training_competition import router as canonical_training_competition_router
 from app.canonical_training_planning import router as canonical_training_planning_router
+from app.coach_athlete_onboarding import router as coach_athlete_onboarding_router
 from app.canonical_self_report import router as canonical_self_report_router
 from app.canonical_professional_assessment import router as canonical_professional_assessment_router
 from app.canonical_professional_validation import router as canonical_professional_validation_router
@@ -69,6 +70,7 @@ app.include_router(integrative_ai_context_router)
 app.include_router(canonical_decision_cycle_router)
 app.include_router(canonical_training_competition_router)
 app.include_router(canonical_training_planning_router)
+app.include_router(coach_athlete_onboarding_router)
 app.include_router(canonical_self_report_router)
 app.include_router(canonical_professional_assessment_router)
 app.include_router(canonical_professional_validation_router)
